e6d5be02db1ce721653004b44f9a8ce4a803edc3
JugalBoro <jboro999@gmail.com>
2023-08-19 03:06:57 +0530

    fixing bugs

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import express from "express";
 import dotenv from "dotenv";
 import colors from "colors";
 import morgan from "morgan";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 import connectDB from "./config/db.js";
 
@@ -24,8 +25,8 @@ app.get("/", (req, res) => {
   res.send("API is running....");
 });
 
-// app.use(notFound);
-// app.use(errorHandler);
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
